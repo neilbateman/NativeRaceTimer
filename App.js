1fe1f4561a6b265068594c320fa7fe5cd6c0fc51
Neil Bateman <46337141+neilbateman@users.noreply.github.com>
2019-10-08 11:28:47 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import RacerScreen from './components/RacerScreen';
+import RacerDetailScreen from './components/RacerDetailScreen';
+import AddRacerScreen from './components/AddRacerScreen';
+import EditRacerScreen from './components/EditRacerScreen';
+import HomeScreen from './components/HomeScreen';
+import RaceScreen from './components/RaceScreen';
+import RaceResult from './components/RaceResult'
+import CurrentRaceScreen from './components/CurrentRaceScreen';
+import 'firebase/auth';
+import { FirebaseAuthProvider, IfFirebaseAuthed } from '@react-firebase/auth'
+import firebase from './Firebase';
+const Stack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Racer: RacerScreen,
+    RacerDetails: RacerDetailScreen,
+    AddRacer: AddRacerScreen,
+    EditRacer: EditRacerScreen,
+    RaceScreen: RaceScreen,
+    RaceResult: RaceResult,
+    CurrentRaceScreen: CurrentRaceScreen
+  },
+  {
+    initialRouteName: 'Home',
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#777777',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+      headerBackTitle: null,
+    },
+  },
+);
+
+const RootStack = createAppContainer(Stack);
+
+
+export default class App extends React.Component {
+  render() {
+    return(
+      <View style={styles.container}>
+        <RootStack />
+        </View>
+
+    )
+
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#777776',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/AddRacerScreen.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react';
+import { StyleSheet, ScrollView, ActivityIndicator, View, TextInput } from 'react-native';
+import { Button } from 'react-native-elements';
+import firebase from '../Firebase';
+
+class AddRacerScreen extends Component {
+  static navigationOptions = {
+    title: 'Add Racer',
+  };
+  constructor() {
+    super();
+    this.ref = firebase.firestore().collection('racers');
+    this.state = {
+      title: '',
+      description: '',
+      author: '',
+      isLoading: false,
+    };
+  }
+  updateTextInput = (text, field) => {
+    const state = this.state
+    state[field] = text;
+    this.setState(state);
+  }
+
+  saveRacer() {
+    this.setState({
+      isLoading: true,
+    });
+    this.ref.add({
+      title: this.state.title,
+      description: this.state.description,
+      author: this.state.author,
+    }).then((docRef) => {
+      this.setState({
+        title: '',
+        description: '',
+        author: '',
+        isLoading: false,
+      });
+      this.props.navigation.goBack();
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+      this.setState({
+        isLoading: false,
+      });
+    });
+  }
+
+  render() {
+    if(this.state.isLoading){
+      return(
+        <View style={styles.activity}>
+          <ActivityIndicator size="large" color="#0000ff"/>
+        </View>
+      )
+    }
+    return (
+      <ScrollView style={styles.container}>
+        <View style={styles.subContainer}>
+          <TextInput
+              placeholder={'Title'}
+              value={this.state.title}
+              onChangeText={(text) => this.updateTextInput(text, 'title')}
+          />
+        </View>
+        <View style={styles.subContainer}>
+          <TextInput
+              multiline={true}
+              numberOfLines={4}
+              placeholder={'Description'}
+              value={this.state.description}
+              onChangeText={(text) => this.updateTextInput(text, 'description')}
+          />
+        </View>
+        <View style={styles.subContainer}>
+          <TextInput
+              placeholder={'Author'}
+              value={this.state.author}
+              onChangeText={(text) => this.updateTextInput(text, 'author')}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            large
+            leftIcon={{name: 'save'}}
+            title='Save'
+            onPress={() => this.saveRacer()} />
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20
+  },
+  subContainer: {
+    flex: 1,
+    marginBottom: 20,
+    padding: 5,
+    borderBottomWidth: 2,
+    borderBottomColor: '#CCCCCC',
+  },
+  activity: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default AddRacerScreen;
